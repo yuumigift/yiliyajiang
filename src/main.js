5ad3965c40f5f8b4a717924bbc0acbb3e17a549d
悠米 <yuumigift@qq.com>
2023-05-10 14:59:43 +0800

    优化use

--- src/main.js
+++ src/main.js
@@ -1,37 +1,35 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 import app from "@/App.vue";
-import '@/assets/font/font.css'
-import '@/assets/css/mouse.css'
+import "@/assets/font/font.css";
+import "@/assets/css/mouse.css";
 import axios from "axios";
-import store from './store';
-import Antd from 'ant-design-vue';
-import antdcss from 'antd/dist/reset.css'
+import store from "./store";
+import Antd from "ant-design-vue";
+import "antd/dist/reset.css";
 //import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 router.beforeEach((to, from, next) => {
-    document.title = `${to.meta.title}`;
-    const role = store.state.data;
-    if (role === '' && to.path !== '/login') {
-        next('/login');
-    } else {
-        if(role && to.path === '/login'){
-            if (role === 0){
-            }else {
-            }
-        }
-        if (role && to.path.includes('hidden')){
-            if (role !== 0){
-                next('/login')
-            }
-        }
-        next();
-
+  document.title = `${to.meta.title}`;
+  const role = store.state.data;
+  if (role === "" && to.path !== "/login") {
+    next("/login");
+  } else {
+    if (role && to.path === "/login") {
+      if (role === 0) {
+      } else {
+      }
     }
-})
+    if (role && to.path.includes("hidden")) {
+      if (role !== 0) {
+        next("/login");
+      }
+    }
+    next();
+  }
+});
 
-createApp(App).use(router,store,Antd).mount('#app')
+createApp(App).use(router).use(store).use(Antd).mount("#app");
 // app.config.globalProperties.initAudio = initAudio    // main.ts/js 文件中
 
-
-app.config.globalProperties.$http=axios
+app.config.globalProperties.$http = axios;
