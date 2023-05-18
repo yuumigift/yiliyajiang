512e9f4abb42db24912224ab123a50a9671c37d5
悠米 <yuumigift@qq.com>
2023-05-18 11:39:13 +0800

    悬挂星星组件

--- src/views/home/CakeView.vue
+++ src/views/home/CakeView.vue
@@ -7,83 +7,34 @@
         <div class="cake">
             <img src="../../assets/images/生日蛋糕.png" alt="" />
         </div>
-        <div class="star_container" style="top: -90px">
-            <div class="line" v-for="(item, index) in stars" style="" :key="index" :style="{ left: `${item.x}px`, top: `${item.y - remain_height}px` }">
-                <div class="line--inner"></div>
-            </div>
-            <div class="stars" v-for="(item, index) in stars" :key="index" :style="{ left: `${item.x}px`, top: `${item.y - remain_height}px` }">
-                <div class="stars--inner">
-                    <video autoplay muted :hidden="hidden">
-                        <source src="../../assets/mp4/星星（生成）.mp4" type="video/mp4" />
-                    </video>
-                    <video autoplay muted loop :hidden="!hidden" @ended="handleEnded(item)">
-                        <source src="../../assets/mp4/星星（完整）.mp4" type="video/mp4" />
-                    </video>
-                </div>
-            </div>
+        <div class="star_container">
+            <OverhangStar :angle="Math.PI" :x="0" :y="50" :speed="13" :freezing="0"></OverhangStar>
+            <OverhangStar :angle="Math.PI" :x="200" :y="100" :speed="16" :freezing="20"></OverhangStar>
+            <OverhangStar :angle="Math.PI" :x="400" :y="0" :speed="11" :freezing="40"></OverhangStar>
+            <OverhangStar :angle="0" :x="600" :y="60" :speed="14" :freezing="60"></OverhangStar>
+            <OverhangStar :angle="0" :x="800" :y="20" :speed="12" :freezing="80"></OverhangStar>
+            <OverhangStar :angle="0" :x="1000" :y="120" :speed="18" :freezing="100"></OverhangStar>
         </div>
         <div class="mask"></div>
     </div>
 </template>
 <script>
 import AddText from "@/components/AddText.vue";
+import OverhangStar from "@/components/OverhangStar.vue";
 
 export default {
-    components: { AddText },
+    components: { AddText, OverhangStar },
     data() {
         return {
-            a_speed: 0.4,
-            remain_height: 200,
-            RANGE: 30, // 星星摇晃幅度
             mus: require("../../assets/audios/泠鸢yousa - 勾指起誓.mp3"),
-            hidden: false,
-            stars: [
-                // speed：星星摇晃频率
-                { y: 50, x: 0, speed: 1.3, shaking: false, angle: Math.PI },
-                { y: 120, x: 0, speed: 1.6, shaking: false, angle: Math.PI },
-                { y: 0, x: 0, speed: 1.4, shaking: false, angle: Math.PI },
-                { y: 60, x: 0, speed: 1.5, shaking: false, angle: 0 },
-                { y: 20, x: 0, speed: 1.7, shaking: false, angle: 0 },
-                { y: 160, x: 0, speed: 1.3, shaking: false, angle: 0 },
-            ],
         };
     },
     mounted() {
         this.$refs.MusicPlay.volume = 0.15;
-        this.enterFrame();
-        setTimeout(() => {
-            this.hidden = true;
-        }, 24500);
     },
     methods: {
-        handleEnded(item) {
-            item.shaking = true;
-        },
         musicPlay() {
             this.$refs.MusicPlay.play();
-        },
-        move(item) {
-            let { x, y, speed, angle, shaking } = item;
-            if (this.remain_height > 0) {
-                this.remain_height -= 0.01;
-            }
-            if (this.hidden) {
-                if (this.a_speed < 1) {
-                    angle += speed * 0.01 * this.a_speed * this.a_speed;
-                    this.a_speed += 0.0005;
-                } else {
-                    angle += speed * 0.01;
-                }
-            }
-            if (angle > Math.PI * 2) {
-                angle -= Math.PI * 2;
-            }
-            x = Math.cos(angle) * this.RANGE;
-            return { x, y, speed, angle, shaking };
-        },
-        enterFrame() {
-            requestAnimationFrame(this.enterFrame);
-            this.stars = this.stars.map((item) => this.move(item));
         },
     },
 };
@@ -141,35 +92,8 @@ video {
     }
 }
 .star_container {
-    position: relative;
     grid-row: 1;
     grid-column: 1;
-    display: grid;
-    grid-template-columns: repeat(6, 1fr);
-    grid-template-rows: 90px auto 1fr;
-}
-.stars {
     position: relative;
-
-    &--inner {
-        position: absolute;
-    }
-}
-
-.line {
-    position: relative;
-    display: grid;
-    transform: translateY(10px);
-
-    &--inner {
-        position: absolute;
-        justify-self: center;
-        width: 1px;
-        height: 500px;
-        bottom: -30px;
-        transform: translateX(2px);
-        border-right: dashed 1px #fff;
-        box-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 15px #fff, 0 0 20px #fff;
-    }
 }
 </style>
